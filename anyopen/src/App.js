5132612e8ea3ec7e526dbf3b809fbc83658a3ab5
Tamanna garg <tgarg661@gmail.com>
2022-01-17 11:22:34 +0530

    added changes in any open accordian

---
+++ anyopen/src/App.js
@@ -0,0 +1,13 @@
+import ACCORDIAN  from './open/accordance';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ACCORDIAN />
+      
+    </div>
+  );
+}
+
+export default App;
